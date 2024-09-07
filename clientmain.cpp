8e819538f168c1dfb09a5e56c397c55c1df85ce2
Gullis477 <emil92@hotmail.se>
2024-09-07 16:09:25 +0200

    sends "OK\n", error prints, operations function.

--- clientmain.cpp
+++ clientmain.cpp
@@ -31,6 +31,45 @@ bool check_protocol(char* buf){
   return false;
 }
 
+bool send_message(int clientSocket, const char* message) {
+    ssize_t bytesSent = send(clientSocket, message, strlen(message), 0);
+    if (bytesSent < 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+void do_magic(char* buf){
+
+  if (strstr(buf, "fdiv") != NULL) {
+      printf("fdiv\n");
+    }
+  else   if (strstr(buf, "fadd") != NULL) {
+      printf("fadd\n");
+    }
+  else   if (strstr(buf, "fmul") != NULL) {
+      printf("fmul\n");
+    }
+  else   if (strstr(buf, "fsub") != NULL) {
+      printf("fsub\n");
+    }
+  else   if (strstr(buf, "fdiv") != NULL) {
+      printf("fdiv\n");
+    }
+  else   if (strstr(buf, "add") != NULL) {
+      printf("add\n");
+    }
+  else   if (strstr(buf, "div") != NULL) {
+      printf("div\n");
+    }
+  else   if (strstr(buf, "mul") != NULL) {
+      printf("mul\n");
+    }
+  else   if (strstr(buf, "sub") != NULL) {
+      printf("sub\n");
+    }
+}
+
 int main(int argc, char *argv[]){
   
   /*
@@ -45,7 +84,7 @@ int main(int argc, char *argv[]){
 
 
 
-  /* Do magic */
+
 
   int port=atoi(Destport);
 
@@ -55,24 +94,31 @@ int main(int argc, char *argv[]){
   serverAddress.sin_family = AF_INET; 
   serverAddress.sin_port = htons(port); 
   inet_pton(AF_INET, Desthost, &serverAddress.sin_addr); 
-  int connectResult = connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-
+  if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+      printf("ERROR: Cannot connect to %s:%d.\n",Desthost,port);
+      return -1;
+  }
 
 
   char buffer[1024];
   memset(buffer, 0, sizeof(buffer));
   ssize_t bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-  if( !check_protocol(buffer))
-  {
+  if(check_protocol(buffer)){
+    send_message(clientSocket, "OK\n");
+  }
+  else{
     printf("ERROR: MISSMATCH PROTOCOL\n");
     close(clientSocket);
   }
 
 
+  memset(buffer, 0, sizeof(buffer));
+  recv(clientSocket, buffer, sizeof(buffer), 0);
+  do_magic(buffer);
 
 #ifdef DEBUG 
   printf("Hhet %s, and port %d.\n",Desthost,port);
 #endif
   close(clientSocket);
-  return connectResult;
+  return 0;
 }
